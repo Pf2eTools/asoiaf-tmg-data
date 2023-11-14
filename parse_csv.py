1bf646443ddcdb3e536a1d4765421976e8b6445c
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2023-11-14 23:10:02 +0100

    - translations (tested with de) - improved text rendering

--- parse_csv.py
+++ parse_csv.py
@@ -13,27 +13,6 @@ def csv_to_dict(path):
     return data
 
 
-def parse_tactics():
-    data = csv_to_dict(f"{CSV_PATH}/tactics.csv")
-    parsed_cards = []
-    for card_data in data:
-        parsed = {
-            "id": card_data.get("Id"),
-            "name": card_data.get("Name").split("\n"),
-            "version": card_data.get("Version"),
-            "faction": card_data.get("Faction"),
-            "text": [parse_ability_trigger(parse_ability_text(p.strip())) for p in card_data.get("Text").split(" /")],
-        }
-        if card_data.get("Remove") != "":
-            parsed["remove"] = card_data.get("Remove")
-        if card_data.get("Unit") != "":
-            parsed["commander_id"] = card_data.get("Unit")
-            parsed["commander_name"] = card_data.get("Deck").split(", ")[0].strip()
-            parsed["commander_subname"] = card_data.get("Deck").split(", ")[1].strip()
-        parsed_cards.append(parsed)
-    return parsed_cards
-
-
 def parse_ability_trigger(paragraphs):
     if len(paragraphs) > 1:
         return {
@@ -49,7 +28,8 @@ def parse_ability_trigger(paragraphs):
 
 
 def parse_ability_text(text):
-    def parse_para(para):
+    def parse_para(para, split_func=None):
+        para = para.strip()
         match = re.match(r"^\[ATTACK:(.*)]$", para)
         if match:
             atk_type, atk_name, atk_stats = match.group(1).split(":")
@@ -60,25 +40,119 @@ def parse_ability_text(text):
                 "hit": int(hit),
                 "dice": [int(d) for d in dice.split(",")]
             }
+        elif split_func is not None:
+            return split_func(para)
         else:
             return [line.strip() for line in para.split("\n")]
 
-    paragraphs = [parse_para(p.strip()) for p in text.split("\n\n")]
+    split_by_double = re.split(r"\n\s*?\n", text.strip())
+    split_by_single = re.split(r"\n+\s*", text.strip())
+    # FIXME/TODO: This heuristic is terrible!
+    if max([max([len(l) for l in p.split("\n")]) for p in split_by_double]) > 50:
+        max_line_len = 38 if len(text) / 32 < 16 else 41
+        split_function = lambda x: split_paragraph(x, max_line_len)
+        paragraphs = [parse_para(p, split_function) for p in split_by_single if p]
+    else:
+        paragraphs = [parse_para(p) for p in split_by_double if p]
 
     return paragraphs
+
+
+def split_paragraph(paragraph_text, max_len=None, ignore_hurenkind=False):
+    max_len = max_len or 38
+    # Split on space or hyphen, but not before a comma
+    words = [w for w in re.split(r"([^\s-]+-)|\s(?!,)", paragraph_text.strip()) if w]
+    lines = []
+    line = []
+    for word in words:
+        len_2 = lambda x: 3 if x.startswith("[") else len(x)
+        len_line = sum([len_2(w) + 1 for w in line]) + len_2(word)
+        if len_line > max_len:
+            if not line:
+                line = [word]
+                lines.append(line)
+                line = []
+            else:
+                lines.append(line)
+                line = [word]
+        else:
+            line.append(word)
+    if line:
+        lines.append(line)
+
+    if not ignore_hurenkind and len(lines) > 1 and len(lines[-1]) == 1 and len(lines[-2]) > 1:
+        word = lines[-2].pop()
+        lines[-1].insert(0, word)
+
+    def join(l):
+        out = ""
+        for w in l:
+            if out == "" or out.endswith("-"):
+                out += w
+            else:
+                out += f" {w}"
+        return out
+    return [join(l) for l in lines]
+
+
+def parse_tactics():
+    translations = [
+        "de",
+    ]
+    data = csv_to_dict(f"{CSV_PATH}/tactics.csv")
+    parsed_cards = {
+        "en": {}
+    }
+    for card_data in data:
+        id = card_data.get("Id")
+        parsed = {
+            "id": id,
+            "name": card_data.get("Name").split("\n"),
+            "version": card_data.get("Version"),
+            "faction": card_data.get("Faction"),
+            "text": [parse_ability_trigger(parse_ability_text(p)) for p in card_data.get("Text").replace("./", ". /").split(" /")],
+        }
+        if card_data.get("Remove") != "":
+            parsed["remove"] = card_data.get("Remove")
+        if card_data.get("Unit") != "":
+            parsed["commander_id"] = card_data.get("Unit")
+            parsed["commander_name"] = card_data.get("Deck").split(", ")[0].strip()
+            parsed["commander_subname"] = card_data.get("Deck").split(", ")[1].strip()
+        parsed_cards["en"][id] = parsed
+    for lang in translations:
+        translated_data = csv_to_dict(f"{CSV_PATH}/tactics.{lang}.csv")
+        parsed_cards[lang] = {}
+        for card_data in translated_data:
+            id = card_data.get("Id")
+            parsing = parsed_cards["en"][id].copy()
+            parsing["name"] = split_paragraph(card_data.get("Name").strip(), max_len=14, ignore_hurenkind=True)
+            parsing["text"] = [parse_ability_trigger(parse_ability_text(p)) for p in
+                               card_data.get("Text").replace("./", ". /").split(" /")]
+            if not re.search(r"deck", card_data.get("Deck")):
+                cmdr_name_split = card_data.get("Deck").split(", ")
+                parsing["commander_name"] = cmdr_name_split[0].strip()
+                if len(cmdr_name_split) == 2:
+                    parsing["commander_subname"] = cmdr_name_split[1].strip()
+                else:
+                    del parsing["commander_subname"]
+            parsed_cards[lang][id] = parsing
+    return parsed_cards
 
 
 def main():
     # In the future, dump this info as JSON (It's useful for TTS).
     tactics = parse_tactics()
-    for ix, t in enumerate(tactics):
-        gen = build_tactics_card(t).convert("RGB")
-        outpath = f"./tactics/{t['id']}.jpg"
-        print(f"Saving \"{' '.join(t['name'])}\" (ix: {ix}) to {outpath}...")
-        gen.save(outpath)
+    for lang, data in tactics.items():
+        # if lang == "en":
+        #     continue
+        for ix, t in enumerate(data.values()):
+            gen = build_tactics_card(t).convert("RGB")
+            outpath = f"./tactics/{lang}/{t['id']}.jpg"
+            print(f"Saving \"{' '.join(t['name'])}\" (ix: {ix}) to {outpath}...")
+            gen.save(outpath)
 
 
-CSV_PATH = "./assets/data"
+CSV_PATH = "./data/warcouncil"
 
 if __name__ == "__main__":
     main()
--- tactics_card_generator.py
+++ tactics_card_generator.py
@@ -70,7 +70,7 @@ def make_attack_bar(atk_type, atk_name, atk_ranks, tohit, border_color="Gold"):
     atk_name_color = "#0e2e45" if atk_type == "melee" else "#87282a"
     text_lines_list = split_on_center_space(atk_name.upper())
     rd_atk_name = render_paragraph(text_lines_list, atk_name_color, 30, 6, font_family="Tuff-Italic",
-                                   thickness_factor=1.8)
+                                   stroke_width=0.55)
     attack_bar.alpha_composite(rd_atk_name, (224 - rd_atk_name.size[0] // 2, 56 - rd_atk_name.size[1] // 2))
 
     if atk_type != "melee":
@@ -87,10 +87,10 @@ def make_attack_bar(atk_type, atk_name, atk_ranks, tohit, border_color="Gold"):
     rank_colors = ["#648f4a", "#dd8e29", "#bd1a2b"]
     for i in range(len(atk_ranks)):
         rank_bg = Image.new("RGBA", (35, 35), rank_colors[i])
-        rd_num_dice = render_text_line(str(atk_ranks[i]), "#cdcecb", 37, "Garamond-Bold", thickness_factor=0)
+        rd_num_dice = render_text_line(str(atk_ranks[i]), "#cdcecb", 37, "Garamond-Bold", stroke_width=0)
 
         # This was an interesting programming journey, but having a transparent font makes it pretty hard to read the text
-        # rd_num_dice = render_text_line(str(atk_ranks[i]), "black", 35, "Garamond-Bold", thickness_factor=0)
+        # rd_num_dice = render_text_line(str(atk_ranks[i]), "black", 35, "Garamond-Bold", stroke_width=0)
         rd_num_dice = rd_num_dice.crop(rd_num_dice.getbbox())
         rank_bg.alpha_composite(rd_num_dice, (16 - rd_num_dice.size[0] // 2, 6))
         # mask = Image.new("RGBA", (35, 35))
@@ -152,30 +152,34 @@ def apply_drop_shadow(image, shadow_size=3, color="black", passes=5):
 
 # TODO: Breaks down for specials
 def calc_height_paragraph(paragraph, font_size, line_padding):
-    return sum([font_size * FACTOR_FIXED_LINE_HEIGHT for _ in paragraph]) + (len(paragraph) - 1) * line_padding
-
+    return (len(paragraph) - 1) * int(font_size * FACTOR_FIXED_LINE_HEIGHT + line_padding) + font_size + 7
 
 def calc_height_paragraphs(paragraphs, font_size, line_padding, paragraph_padding):
     h = [calc_height_paragraph(p, font_size, line_padding) for p in paragraphs]
-    return sum(h) + (len(h) - 1) * (paragraph_padding + 7)
+    return sum(h) + (len(h) - 1) * paragraph_padding
 
 
-def text_to_image(text, font_path, font_size, font_color, thickness_factor=3, letter_spacing=0):
-    # The stroke_width property (see below) would be really nice if it worked with smaller font sizes
+def text_to_image(text, font_path, font_size, font_color, stroke_width=0.35, letter_spacing=0, max_width=0):
+    # The stroke_width property (see below) would be really nice if it worked with smaller font sizes (or floats)
     # As a workaround, we use a bigger fontsize, then downscale the image later
-    stroke_width = 0 if thickness_factor == 0 else 1
-    thickness_factor = thickness_factor or 1
-    font = ImageFont.truetype(font_path, int(font_size * thickness_factor))
+    scale = 1 if int(stroke_width) == stroke_width else 1 / (stroke_width % 1)
+    stroke_width = 0 if stroke_width == 0 else int(stroke_width / (stroke_width % 1))
+
+    font = ImageFont.truetype(font_path, int(font_size * scale))
     bbox = font.getbbox(text)
-    # Offset of 1, otherwise tall letters are clipped
-    offset_x, offset_y = 0, 1
+    # Dynamically adjust the letter-spacing if the text is too wide
+    if max_width != 0 and bbox[2] > max_width * scale:
+        letter_spacing -= min(1.1, ((bbox[2] - max_width * scale) * 1.05) / len(text))
+
+    # Offset, otherwise tall letters are clipped, Umlaute (ÖÄÜ) in particular
+    offset_x, offset_y = 0, 1 + int(7 * scale)
     # For vertical center alignment, ensure the height is consistent. "yjg" are the tallest letters.
-    # Usually, this means h == font_size
-    w, h = bbox[2] + int((len(text) - 1) * letter_spacing), font.getbbox("yjg")[3] + 1
-    # Avoid clipping of letters such as "j"
+    # Usually, this means h == font_size + 7 after scaling the image back to size
+    w, h = bbox[2] + int((len(text) - 1) * letter_spacing), font.getbbox("yjg")[3] + 1 + int(7 * scale)
+    # Avoid clipping of letters such as "j" at the start of the text
     if bbox[0] < 0:
         w -= bbox[0]
-        offset_x += bbox[0]
+        offset_x -= bbox[0]
     canvas = Image.new('RGBA', (w, h), (0, 0, 0, 0))
     draw = ImageDraw.Draw(canvas)
 
@@ -187,7 +191,8 @@ def text_to_image(text, font_path, font_size, font_color, thickness_factor=3, le
         offset_next = font.getlength(char + char_next) - font.getlength(char_next)
         offset_x += offset_next + letter_spacing
 
-    return canvas.resize((int(canvas.size[0] / thickness_factor), int(canvas.size[1] / thickness_factor)))
+    canvas = canvas.resize((int(canvas.size[0] / scale), int(canvas.size[1] / scale)))
+    return canvas
 
 
 def render_text_icon(token, font_color, font_size):
@@ -235,7 +240,7 @@ def render_text_line(line, font_color, font_size, font_family=None, font_style=N
             continue
         elif token.startswith("["):
             rendered = render_text_icon(token, font_color, font_size)
-            rendered_tokens.append({"im": rendered, "type": "icon"})
+            rendered_tokens.append(rendered)
         elif token == "*":
             font_style["is_italic"] = not font_style["is_italic"]
         elif token == "**":
@@ -246,23 +251,16 @@ def render_text_line(line, font_color, font_size, font_family=None, font_style=N
             font_style_str = font_style_str or "Normal"
             font_path = f"./fonts/Tuff-{font_style_str}.ttf" if font_family is None else f"./fonts/{font_family}.ttf"
             rendered = text_to_image(token, font_path, font_size, font_color, **kwargs)
-            rendered_tokens.append({"im": rendered, "type": "text"})
+            rendered_tokens.append(rendered)
 
-    total_width = sum([tkn["im"].size[0] for tkn in rendered_tokens])
-    max_height = max([tkn["im"].size[1] for tkn in rendered_tokens])
-    rendered_line = Image.new("RGBA", (total_width, max_height + 7))
+    total_width = sum([image.size[0] for image in rendered_tokens])
+    max_height = max([image.size[1] for image in rendered_tokens])
+    rendered_line = Image.new("RGBA", (total_width, max_height))
     x = 0
-    for token in rendered_tokens:
-        image = token["im"]
-        # It's disgusting, but icons need to be rendered higher than the font. So we pad each text line by 7px.
-        if token["type"] == "icon":
-            rendered_line.paste(image, (x, 0), image)
-        else:
-            rendered_line.paste(image, (x, 7), image)
+    for image in rendered_tokens:
+        rendered_line.paste(image, (x, 0), image)
         x += image.size[0]
 
-    out = Image.new("RGBA", rendered_line.size, "red")
-    out.alpha_composite(rendered_line)
     return rendered_line
 
 
@@ -299,7 +297,7 @@ def render_paragraph(paragraph, font_color, font_size, padding_lines, **kwargs):
     return combine_images_horizontal_center(rendered_lines, padding_lines, int(font_size * FACTOR_FIXED_LINE_HEIGHT))
 
 
-def render_paragraphs(paragraphs, font_color, font_size, line_padding, padding_paragraph):
+def render_paragraphs(paragraphs, font_color, font_size, line_padding, padding_paragraph, **kwargs):
     rendered_paragraphs = []
     offsets = []
     for paragraph in paragraphs:
@@ -323,7 +321,7 @@ def render_paragraphs(paragraphs, font_color, font_size, line_padding, padding_p
                 color = font_color
                 size = font_size
                 padding = line_padding
-            rendered_paragraph = render_paragraph(content, color, size, padding)
+            rendered_paragraph = render_paragraph(content, color, size, padding, **kwargs)
         rendered_paragraphs.append(rendered_paragraph)
         offsets.append(offset)
 
@@ -373,10 +371,10 @@ def build_tactics_card(card_info):
 
     if commander_id is None:
         bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1])), (46, 336))
-        bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1])), (692, 336))
+        bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1])), (687, 336))
     else:
         bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1] + 82)), (46, 246))
-        bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1] + 82)), (692, 246))
+        bars.alpha_composite(small_bar.rotate(90, expand=1).crop((0, 0, 100, tactics_bg2.size[1] + 82)), (687, 246))
     bars.alpha_composite(small_bar.crop((0, 0, tactics_bg2.size[0], 100)),
                          ((tactics_bg.size[0] - tactics_bg2.size[0]) // 2, 985))
     bars.alpha_composite(small_bar, (0, 246))
@@ -384,32 +382,37 @@ def build_tactics_card(card_info):
 
     decor = Image.open(f"{ASSETS_DIR}/Tactics/Decor{faction}.webp").convert('RGBA')
     bars.alpha_composite(decor, (33, 316))
-    bars.alpha_composite(decor, (678, 316))
+    bars.alpha_composite(decor, (673, 316))
     bars.alpha_composite(decor, (33, 971))
-    bars.alpha_composite(decor, (678, 971))
+    bars.alpha_composite(decor, (673, 971))
     if commander_id is not None:
         bars.alpha_composite(decor, (33, 232))
-        bars.alpha_composite(decor, (678, 232))
+        bars.alpha_composite(decor, (673, 232))
 
     all_text = Image.new('RGBA', tactics_bg.size)
 
     rendered_name = render_paragraph([f"**{l.upper()}**" for l in name], font_color="white", font_size=51,
                                      padding_lines=12)
+    name_max_w = 440 if commander_name is None else 392
+    if rendered_name.getbbox()[2] > name_max_w:
+        rendered_name = rendered_name.resize((name_max_w, int(rendered_name.size[1] * name_max_w / rendered_name.size[0])))
     if commander_name is not None:
-        full_commander_name = f"**{commander_name}** - {commander_subname}".upper()
+        full_commander_name = f"**{commander_name}**".upper()
+        full_commander_name += f" - {commander_subname}".upper() if commander_subname is not None else ""
         letter_spacing = 0 if len(full_commander_name) < 36 else -1.5
-        font_size = 32 if len(full_commander_name) < 45 else 29
-        if len(full_commander_name) < 48:
+        font_size = 32 if len(full_commander_name) < 43 else 29
+        if len(full_commander_name) < 46 or commander_subname is None:
             rendered_cmdr_name = render_text_line(full_commander_name, "white", font_size, letter_spacing=letter_spacing)
         else:
+            # yeah, that's not happening if commander_subname == None
             rendered_cmdr_name = render_paragraph([f"**{commander_name.upper()}**", commander_subname.upper()], "white", 29, 4)
         cmdr_x, cmdr_y = (tactics_bg.size[0] - rendered_cmdr_name.size[0]) // 2, 294 - rendered_cmdr_name.size[1] // 2
         all_text.alpha_composite(rendered_cmdr_name, (cmdr_x, cmdr_y))
-        all_text.alpha_composite(rendered_name, (
-            (tactics_bg.size[0] - rendered_name.size[0]) // 2 + 162, 136 - rendered_name.size[1] // 2))
+        name_x, name_y = (tactics_bg.size[0] - rendered_name.size[0]) // 2 + 170, 136 - rendered_name.size[1] // 2
+        all_text.alpha_composite(rendered_name, (name_x, name_y))
     else:
-        all_text.alpha_composite(rendered_name, (
-            (tactics_bg.size[0] - rendered_name.size[0]) // 2 + 128, 140 - rendered_name.size[1] // 2))
+        name_x, name_y = (tactics_bg.size[0] - rendered_name.size[0]) // 2 + 138, 140 - rendered_name.size[1] // 2
+        all_text.alpha_composite(rendered_name, (name_x, name_y))
 
     card_text_y = 360
     font_size = 41
@@ -418,36 +421,53 @@ def build_tactics_card(card_info):
     total_text_height = sum([calc_height_paragraph(te.get("trigger"), 41, 18) for te in card_text])
     total_text_height += sum([calc_height_paragraphs(te.get("effect"), 41, 18, 16) for te in card_text])
     # height of the bars
-    total_text_height += (len(card_text) - 1) * (60 + small_bar.size[1])
+    total_text_height += (len(card_text) - 1) * (18 + paragraph_padding + small_bar.size[1])
     # height of the space between trigger and effect
     total_text_height += len(card_text) * (paragraph_padding - 4)
 
-    if total_text_height > 560:
-        line_padding = 16
-    if total_text_height > 570:
-        paragraph_padding = 12
-    if total_text_height > 580:
-        line_padding = 14
-    if total_text_height > 590:
-        paragraph_padding = 8
-    if total_text_height > 600:
-        line_padding = 12
-    if total_text_height > 610:
-        font_size = 38
-    if total_text_height > 620:
-        paragraph_padding = 6
-    # might need more aggressive measures in the future
+    # text taller than this will clip
+    max_text_height = 600
+    text_overflow_px = total_text_height - max_text_height
+    if text_overflow_px > 0:
+        num_lines = 0
+        num_paragraphs = 0
+        for te in card_text:
+            if te.get("trigger") is not None:
+                num_paragraphs += 1
+                for l in te.get("trigger"):
+                    num_lines += 1
+            for p in te.get("effect") or []:
+                num_paragraphs += 1
+                for l in p:
+                    num_lines += 1
+
+        if text_overflow_px >= 100:
+            # allow getting closer to the edge
+            card_text_y -= 12
+            text_overflow_px -= 12
+        if text_overflow_px >= 60:
+            font_size -= 3
+            text_overflow_px -= int(num_lines * FACTOR_FIXED_LINE_HEIGHT * 3)
+        for i in range(15):
+            if text_overflow_px <= 0:
+                break
+            if i % 2 == 0:
+                line_padding -= 1
+                text_overflow_px -= num_lines
+            else:
+                paragraph_padding -= 2
+                text_overflow_px -= 2 * num_paragraphs
 
     for ix, trigger_effect in enumerate(card_text):
         trigger = trigger_effect.get("trigger")
         effect = trigger_effect.get("effect")
         is_remove_text = ix > 0 and card_info.get("remove") is not None
 
-        should_render_low = is_remove_text or (total_text_height < 540 and ix == 1)
+        should_render_low = is_remove_text or (ix == len(card_text) - 1 and ix > 0)
         font_color = get_faction_color(faction) if not is_remove_text else "#5d4d40"
-        rd_trigger = render_paragraph(trigger, font_color, font_size, line_padding)
+        rd_trigger = render_paragraph(trigger, font_color, font_size, line_padding, max_width=590)
         if not is_remove_text:
-            rd_effect_text = render_paragraphs(effect, "#5d4d40", font_size, line_padding, paragraph_padding)
+            rd_effect_text = render_paragraphs(effect, "#5d4d40", font_size, line_padding, paragraph_padding, max_width=590)
         else:
             rd_effect_text = Image.new("RGBA", (0,0))
         if should_render_low:
@@ -461,7 +481,7 @@ def build_tactics_card(card_info):
             bars.alpha_composite(small_bar.crop((0, 0, tactics_bg2.size[0], 100)),
                                  ((tactics_bg.size[0] - tactics_bg2.size[0]) // 2, card_text_y))
             bars.alpha_composite(decor, (33, card_text_y + (small_bar.size[1] - decor.size[1]) // 2))
-            bars.alpha_composite(decor, (678, card_text_y + (small_bar.size[1] - decor.size[1]) // 2))
+            bars.alpha_composite(decor, (673, card_text_y + (small_bar.size[1] - decor.size[1]) // 2))
             card_text_y += small_bar.size[1] + paragraph_padding + 4
 
         # finally paste the text
@@ -598,7 +618,7 @@ def main():
     }
 
     tactics_card = build_tactics_card(intrigue)
-    tactics_card_original = Image.open("intrigue.jpg")
+    tactics_card_original = Image.open("tactics/en/40101.jpg")
     app = ImageEditor(tactics_card, tactics_card_original)
 
 
