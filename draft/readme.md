586fe6544bbfc1ee61e73a205928aea5eeffbeaf
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2025-02-19 15:18:31 +0100

    draft

---
+++ draft/readme.md
@@ -0,0 +1,26 @@
+# ASOIAF:TMG - DRAFT
+
+Draft armies using https://draftmancer.com by uploading `cube.txt` as a custom card list.
+
+### Draft procedure
+We use packs of size 10 as an example.
+1. Packs that contain everything you need to create an asoiaf army are randomly generated. Each pack contains 10 items (combat units, ncus, commanders, attachments, and basedecks).
+2. Each player is assigned a pack. The contents of the packs are hidden to the other players.
+3. Each player looks at the content of their pack, secretly chooses an item, and returns the other items to the pack.
+4. Once all players have made their choice, each player passes their pack on to their neighbor.
+5. Repeat steps 3 and 4 until the packs are empty.
+6. Repeat steps 2-5. At the end of this step, each player should have picked 20 items.
+7. Each player reveals their picks.
+8. Each player builds a single 40 point army from the 20 items they picked. There are additional listbuilding rules detailed below.
+9. Reset everything, then repeat steps 1-8 for each player to build their second list.
+
+### Suggested Draft Rules
+For these rules, "item" refers to any combat unit, ncu, attachment, commander, or basedeck.
+- Your list may include items from any number of factions. There is no neutral point limit.
+- You may ignore rules that require you to field other characters. *(For example, you may select dragons without Dany.)*
+- If an item has a "Loyalty" rule, you may ignore that rule.
+- If a combat unit requires you to attach a specific character, you may ignore that rule if you don't include that character as an attachment in your list.
+- For all purposes except "Raiders of the Iron Islands" (Pillage), each of your combat units counts as belonging to every faction.
+- Balon NCU can only use his abilty on Infantry units.
+- If your commander requires you to remove a card from your tactics deck, and you can't remove that card, you may choose which card to remove before each game. You must show your opponent the card you removed.
+
---
+++ py/draft.py
@@ -0,0 +1,186 @@
+import json
+from const import *
+from PIL import Image
+import os
+
+
+DIST = {
+    "commander": 19, 
+    "unit": 78, 
+    "ncu": 48, 
+    "attachment": 37, 
+    "basedeck": 18, 
+}
+
+PROP_TO_MAGIC_TYPE = {
+    "units": "Unit",
+    "ncus": "NCU",
+    "attachments": "Attachment",
+    "basedeck": "Base Deck",
+    "commander": "Commander",
+    "default": "Unknown"
+}
+
+BANNED_IDS = [
+    "20330", # varamyrs eagle
+    "10310", # bear
+    "20308", # eagle 
+    "20309", # wolf
+    "10316", # borroq's boar
+    "30712", # alicent
+    "30713", # rhaenyra 
+    "30404", # seneschal 
+]
+
+def get_data():
+    data = {
+        "commander": [],
+        "unit": [],
+        "ncu": [],
+        "attachment": [],
+    }
+    for faction in FACTIONS:
+        with open(f"./data/en/{faction}.json", "r", encoding="utf-8") as f:
+            fdata = json.load(f)
+        for key, items in fdata.items():
+            for item in items:
+                if item.get("id") in BANNED_IDS:
+                    continue
+                item["prop"] = key
+                if item.get("statistics").get("commander"):
+                    data["commander"].append(item)
+                elif key == "units":
+                    data["unit"].append(item)
+                elif key == "attachments":
+                    data["attachment"].append(item)
+                elif key == "ncus":
+                    data["ncu"].append(item)
+
+    return data
+
+
+def get_basedeck(faction):
+    return {
+        "name": f"{faction}Basedeck",
+        "mana_cost": "{0}",
+        "image": f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/generated/en/{faction}/{faction}.png",
+        "type": PROP_TO_MAGIC_TYPE["basedeck"],
+    }
+
+
+def get_full_name(unt):
+    full_name = unt.get("name")
+    if unt.get("subname"):
+        full_name = f"{full_name} - {unt.get('subname')}"
+    return full_name
+
+
+def get_unit(unt):
+    cost = unt.get("statistics").get("cost", 0)
+    cost = 0 if cost == "C" else cost
+    unit_type = "commander" if unt.get("statistics").get("commander") else unt.get("prop")
+    unit_type = PROP_TO_MAGIC_TYPE.get(unit_type, PROP_TO_MAGIC_TYPE["default"])
+    unit_type_front = f"{unit_type} - Battle" if unt.get("prop") == "units" else unit_type
+    img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/generated/en/{unt.get('statistics').get('faction')}/{unt.get('id')}.jpg"
+    if unt.get("prop") == "units":
+        img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/draft/img/{unt.get('id')}.jpg"
+    full_name = get_full_name(unt)
+
+    return {
+        "name": f"{full_name} ({unt.get('id')})",
+        "mana_cost": f"{{{cost}}}",
+        "image": img_url,
+        "type": unit_type_front,
+        "layout": "split",
+        "printed_names": {
+            "en": unt.get("id"),
+        },
+        "back": {
+            "name": f"{full_name} ({unt.get('id')})",
+            "type": unit_type,
+            "image": img_url.replace(".jpg", "b.jpg"),
+            "layout": "split"
+        }
+    }
+
+
+def get_settings():
+    return {
+        "name": "ASOIAF: TMG",
+        "withReplacement": False,
+        "colorBalance": False,
+        "cardBack": "https://steamusercontent-a.akamaihd.net/ugc/786378723752096342/8B70BD5C3F78C9C5C124F529E4447CC8C1621686/",
+        "layouts": {
+            "Default": {
+                "weight": 1,
+                "slots": [
+                    {
+                        "name": f"Item{i}",
+                        "count": 1,
+                        "sheets": [
+                            {"name": k, "weight": v} for k, v in DIST.items()
+                        ]
+                    } for i in range(10)
+                ]
+            }
+        },
+    }
+
+
+def get_cube_text():
+    data = get_data()
+    cube_text = "[Settings]\n"
+    cube_text += json.dumps(get_settings(), indent=4)
+
+    cube_text += "\n[CustomCards]\n[\n"
+    custom_cards = [json.dumps(get_basedeck(f)) for f in FACTIONS]
+    for items in data.values():
+        custom_cards += [json.dumps(get_unit(u)) for u in items]
+    cube_text += ",\n".join(custom_cards)
+    cube_text += "\n]\n"
+
+    cube_text += "[basedeck]\n"
+    for faction in FACTIONS:
+        cube_text += f"999 {faction}Basedeck\n"
+    for key, items in data.items():
+        cube_text += f"[{key}]\n"
+        for item in items:
+            cmdr = item.get("statistics").get("commander", False)
+            char = item.get("statistics").get("character", False)
+            max_picks = 1 if cmdr else 5
+            cube_text += f"{max_picks} {get_full_name(item)} ({item.get('id')})\n"
+
+    return cube_text
+
+
+def rotate_images(overwrite=False):
+    for faction in FACTIONS:
+        with open(f"./data/en/{faction}.json", "r", encoding="utf-8") as f:
+            fdata = json.load(f)
+        for unit in fdata["units"]:
+            uid = unit.get("id")
+            faction = unit.get("statistics").get("faction")
+            outpath = f"./draft/img/{uid}.jpg"
+            if overwrite or not os.path.exists(outpath):
+                im = Image.open(f"./generated/en/{faction}/{uid}.jpg").rotate(270, expand=1)
+                im.save(outpath)
+                print(f"Saving '{outpath}'...")
+            outpath_b = f"./draft/img/{uid}b.jpg"
+            if overwrite or not os.path.exists(outpath_b):
+                im = Image.open(f"./generated/en/{faction}/{uid}b.jpg").rotate(270, expand=1)
+                im.save(outpath_b)
+                print(f"Saving '{outpath_b}'...")
+
+
+def main(prepare_cube=True, prepare_img=True):
+    if prepare_cube:
+        txt = get_cube_text()
+        with open("./draft/cube.txt", "w", encoding="utf-8") as f:
+            print("Writing to './draft/cube.txt'...")
+            f.write(txt)
+    if prepare_img:
+        rotate_images()
+
+
+if __name__ == "__main__":
+    main()
