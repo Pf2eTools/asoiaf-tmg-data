53a167d49318329432b59e5548cbe0e62569542d
Pf2eTools <78115075+Pf2eTools@users.noreply.github.com>
2025-06-04 10:56:00 +0200

    updated draft

--- py/draft.py
+++ py/draft.py
@@ -1,8 +1,7 @@
 import json
 from PIL import Image
 import os
-
-from py.song_data import FACTIONS
+from py.song_data import FACTIONS, DataLoader
 
 DIST = {
     "commander": 21,
@@ -13,9 +12,9 @@ DIST = {
 }
 
 PROP_TO_MAGIC_TYPE = {
-    "units": "Unit",
-    "ncus": "NCU",
-    "attachments": "Attachment",
+    "unit": "Unit",
+    "ncu": "NCU",
+    "attachment": "Attachment",
     "basedeck": "Base Deck",
     "commander": "Commander",
     "default": "Unknown"
@@ -41,21 +40,20 @@ def get_data():
         "attachment": [],
     }
     for faction in FACTIONS:
-        with open(f"./data/en/{faction}.json", "r", encoding="utf-8") as f:
-            fdata = json.load(f)
-        for key, items in fdata.items():
-            for item in items:
-                if item.get("id") in BANNED_IDS:
-                    continue
-                item["prop"] = key
-                if item.get("statistics").get("commander"):
-                    data["commander"].append(item)
-                elif key == "units":
-                    data["unit"].append(item)
-                elif key == "attachments":
-                    data["attachment"].append(item)
-                elif key == "ncus":
-                    data["ncu"].append(item)
+        structured = DataLoader.load_structured(f"./data/en/{faction}.json")
+        for item in structured.all_entities:
+            if item.id in BANNED_IDS:
+                continue
+            if item.role in ["tactics", "special"]:
+                continue
+            if item.commander:
+                data["commander"].append(item)
+            elif item.role == "unit":
+                data["unit"].append(item)
+            elif item.role == "attachment":
+                data["attachment"].append(item)
+            elif item.role == "ncu":
+                data["ncu"].append(item)
 
     return data
 
@@ -69,36 +67,35 @@ def get_basedeck(faction):
     }
 
 
-def get_full_name(unt):
-    full_name = unt.get("name")
-    if unt.get("subname"):
-        full_name = f"{full_name} - {unt.get('subname')}"
+def get_full_name(ent):
+    full_name = ent.name
+    if ent.title:
+        full_name = f"{full_name} - {ent.title}"
     return full_name
 
 
-def get_unit(unt):
-    cost = unt.get("statistics").get("cost", 0)
-    cost = 0 if cost == "C" else cost
-    unit_type = "commander" if unt.get("statistics").get("commander") else unt.get("prop")
+def get_unit(ent):
+    cost = ent.cost
+    unit_type = "commander" if ent.commander else ent.role
     unit_type = PROP_TO_MAGIC_TYPE.get(unit_type, PROP_TO_MAGIC_TYPE["default"])
-    unit_type_front = f"{unit_type} - Battle" if unt.get("prop") == "units" else unit_type
-    img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/generated/en/{unt.get('statistics').get('faction')}/{unt.get('id')}.jpg"
-    if unt.get("prop") == "units":
-        img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/draft/img/{unt.get('id')}.jpg"
-    full_name = get_full_name(unt)
-    layout = "split" if unt.get("prop") == "units" else ""
+    unit_type_front = f"{unit_type} - Battle" if ent.role == "unit" else unit_type
+    img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/generated/en/{ent.faction}/{ent.id}.jpg"
+    if ent.role == "unit":
+        img_url = f"https://raw.githubusercontent.com/Pf2eTools/asoiaf-tmg-data/refs/heads/master/draft/img/{ent.id}.jpg"
+    full_name = get_full_name(ent)
+    layout = "split" if ent.role == "unit" else ""
 
     return {
-        "name": f"{full_name} ({unt.get('id')})",
+        "name": f"{full_name} ({ent.id})",
         "mana_cost": f"{{{cost}}}",
         "image": img_url,
         "type": unit_type_front,
         "layout": layout,
         "printed_names": {
-            "en": unt.get("id"),
+            "en": ent.id,
         },
         "back": {
-            "name": f"{full_name} ({unt.get('id')})",
+            "name": f"{full_name} ({ent.id})",
             "type": unit_type,
             "image": img_url.replace(".jpg", "b.jpg"),
             "layout": layout
@@ -147,29 +144,26 @@ def get_cube_text():
     for key, items in data.items():
         cube_text += f"[{key}]\n"
         for item in items:
-            cmdr = item.get("statistics").get("commander", False)
-            char = item.get("statistics").get("character", False)
+            cmdr = item.commander or False
+            char = item.character or False
             max_picks = 1 if cmdr else 5
-            cube_text += f"{max_picks} {get_full_name(item)} ({item.get('id')})\n"
+            cube_text += f"{max_picks} {get_full_name(item)} ({item.id})\n"
 
     return cube_text
 
 
 def rotate_images(overwrite=False):
     for faction in FACTIONS:
-        with open(f"./data/en/{faction}.json", "r", encoding="utf-8") as f:
-            fdata = json.load(f)
-        for unit in fdata["units"]:
-            uid = unit.get("id")
-            faction = unit.get("statistics").get("faction")
-            outpath = f"./draft/img/{uid}.jpg"
+        structured = DataLoader.load_structured(f"./data/en/{faction}.json")
+        for unit in structured.unit:
+            outpath = f"./draft/img/{unit.id}.jpg"
             if overwrite or not os.path.exists(outpath):
-                im = Image.open(f"./generated/en/{faction}/{uid}.jpg").rotate(90, expand=1)
+                im = Image.open(f"./generated/en/{faction}/{unit.id}.jpg").rotate(90, expand=1)
                 im.save(outpath)
                 print(f"Saving '{outpath}'...")
-            outpath_b = f"./draft/img/{uid}b.jpg"
+            outpath_b = f"./draft/img/{unit.id}b.jpg"
             if overwrite or not os.path.exists(outpath_b):
-                im = Image.open(f"./generated/en/{faction}/{uid}b.jpg").rotate(90, expand=1)
+                im = Image.open(f"./generated/en/{faction}/{unit.id}b.jpg").rotate(90, expand=1)
                 im.save(outpath_b)
                 print(f"Saving '{outpath_b}'...")
 
